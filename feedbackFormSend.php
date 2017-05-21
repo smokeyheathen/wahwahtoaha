4bb150e8eae54493e267033de3c300047a07c026
smokeyheathen <smokeyheathen@gmail.com>
2017-05-21 14:09:10 +0100

    Adding feedbackform php

---
+++ feedbackFormSend.php
@@ -0,0 +1,81 @@
+<?php
+if(isset($_POST['ffEmail'])) {
+
+    // CHANGE THE TWO LINES BELOW
+    $email_to = "llewellyn_jenkins@yahoo.co.uk";
+
+    $email_subject = "Web feedback";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+
+    // validation expected data exists
+    if(!isset($_POST['ffName']) ||
+        !isset($_POST['ffEmail']) ||
+        !isset($_POST['ffMessage'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['ffName']; // required
+    $email_from = $_POST['ffEmail']; // required
+    $message = $_POST['ffMessage']; // required
+    $phrase = $_POST['ffPhrase'];
+    $translation = $_POST['ffTranslation'];
+    $languagepair = $_POST['ffLanguagePair'];
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+  if(strlen($message) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+    $email_message .= "First Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    if(isset($_POST['ffPhrase'])){$email_message .= "Phrase: " .clean_string($phrase)."\n";}
+    if(isset($_POST['ffTranslation'])){$email_message .= "Translation: " .clean_string($translation)."\n";}
+    if(isset($_POST['ffLanguagePair'])){$email_message .= "Language Pair: " .clean_string($languagepair)."\n";}
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+//$headers = 'From: adam@wahwahtoaha.com\r\n'.
+//'Reply-To: adam@wahwahtoaha.com\r\n' .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+//@mail('adam@wahwahtoaha.com', $email_subject, $email_message, $headers);
+?>
+
+<!-- place your own success html below -->
+<html>
+
+<body>
+<p>  Thank you for contacting us. We will be in touch with you very soon.</p>
+</body.
+</html>
+<?php
+}
+die();
+?>
